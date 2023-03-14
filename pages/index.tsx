d29165c5f5bd6234e212c5b82618b8153d1f9b76
Takahiro-JP <matsumotokixt1@gmail.com>
2023-03-14 10:28:32 +0900

    サーバーサイドで画像を取得するように変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,60 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import {GetServerSideProps, NextPage } from "next";
+import { useState } from "react"; 
+import styles from "./index.module.css"
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+// getServerSidePropsからから渡されるpropsの型
+type Props = {
+    initialImageUrl: string;
+};
 
-export default IndexPage
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+    // useStateを使って状態を定義する
+    const [imageUrl, setImageUrl] = useState(initialImageUrl); // 初期値を渡す
+    const [loading, setLoading] = useState(false); // 初期状態はfalseにしておく
+    // マウント時に画像を読み込む宣言
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url); // 画像URLの状態を更新する
+    //         setLoading(false); // ローディング状態を更新する
+    //     });
+    // }, []);
+    // // ボタンをクリックしたときに画像を読み込む処理
+    const handleClick = async () => {
+        setLoading(true);
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLの状態を更新する
+        setLoading(false); // 読み込み中フラグを下げる
+    };
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>
+                他の猫も見る
+                </button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} />}
+            </div>
+        </div>
+    );
+};
+export default IndexPage;
+
+// サーバーサイドで実行する処理
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+type Image = {
+    url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
